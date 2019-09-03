08b6e6d931e05715d749924c73b491535779d287
Haider Hussain <haahussain@gmail.com>
2019-09-03 11:20:07 -0400

    come on

---
+++ week-2/SQL/file1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 15:16:27 2019
+
+@author: flatironschool
+"""
+
+host = 'flatiron.cg3086udjcyp.us-east-2.rds.amazonaws.com'
+user= 'haahussain'
+password = 'andleeb786$'
